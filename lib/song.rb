310308127ee66afb063c7e777a5f14671989dd5a
Adam Clayman <adam@clayman.org>
2018-09-06 01:09:33 +0000

    Done.

---
+++ lib/song.rb
@@ -0,0 +1,25 @@
+class Song
+  attr_accessor :name
+
+  @@all = []
+
+  def initialize(name)
+    @name = name
+  end
+
+  def self.all
+    @@all
+  end
+
+  def self.create(name)
+    Song.new(name).save
+  end
+
+  def save
+    @@all << self
+  end
+
+  def self.destroy_all
+    @@all.clear
+  end
+end
