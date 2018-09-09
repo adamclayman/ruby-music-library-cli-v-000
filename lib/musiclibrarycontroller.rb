53f910bd5f53dbb50a38bace35b03af68185f38b
Adam Clayman <adam@clayman.org>
2018-09-09 19:59:44 +0000

    Done.

--- lib/musiclibrarycontroller.rb
+++ lib/musiclibrarycontroller.rb
@@ -11,34 +11,33 @@ class MusicLibraryController
 
   def call
     i = "input"
-    until i == "exit"
-      puts "Welcome to your music library!"
-      puts "To list all of your songs, enter 'list songs'."
-      puts "To list all of the artists in your library, enter 'list artists'."
-      puts "To list all of the genres in your library, enter 'list genres'."
-      puts "To list all of the songs by a particular artist, enter 'list artist'."
-      puts "To list all of the songs of a particular genre, enter 'list genre'."
-      puts "To play a song, enter 'play song'."
-      puts "To quit, type 'exit'."
-      puts "What would you like to do?"
-      i = gets.chomp
-    end
-
     case i
-    when "exit"
-
-    else
-      puts "Welcome to your music library!"
-      puts "To list all of your songs, enter 'list songs'."
-      puts "To list all of the artists in your library, enter 'list artists'."
-      puts "To list all of the genres in your library, enter 'list genres'."
-      puts "To list all of the songs by a particular artist, enter 'list artist'."
-      puts "To list all of the songs of a particular genre, enter 'list genre'."
-      puts "To play a song, enter 'play song'."
-      puts "To quit, type 'exit'."
-      puts "What would you like to do?"
-      i = gets.chomp
-    end
+      when "list songs"
+        self.list_songs
+      when "list artists"
+        self.list_artists
+      when "list genres"
+        self.list_genres
+      when "list artist"
+        self.list_songs_by_artist
+      when "list genre"
+        self.list_songs_by_genre
+      when "play song"
+        self.play_song
+      when "exit"
+        # Exit / Break from Case Statement
+      else
+        puts "Welcome to your music library!"
+        puts "To list all of your songs, enter 'list songs'."
+        puts "To list all of the artists in your library, enter 'list artists'."
+        puts "To list all of the genres in your library, enter 'list genres'."
+        puts "To list all of the songs by a particular artist, enter 'list artist'."
+        puts "To list all of the songs of a particular genre, enter 'list genre'."
+        puts "To play a song, enter 'play song'."
+        puts "To quit, type 'exit'."
+        puts "What would you like to do?"
+        i = gets.chomp
+      end
   end
 
   def list_songs
@@ -87,7 +86,7 @@ class MusicLibraryController
   def play_song
     self.list_songs
     song_number = 0
-    until ((song_number.is_a? Integer ) && (song_number >= 1) && (song_number <= Song.all.length))
+    unless ((song_number.is_a? Integer ) && (song_number >= 1) && (song_number <= Song.all.length))
       puts "Which song number would you like to play?"
       song_number = gets.chomp
     end
