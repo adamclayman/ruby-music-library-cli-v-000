3bbcfe220abb30fc4b9ad40a0a051c97561b2e94
Adam Clayman <adam@clayman.org>
2018-09-09 18:38:31 +0000

    Done.

--- lib/musiclibrarycontroller.rb
+++ lib/musiclibrarycontroller.rb
@@ -17,7 +17,7 @@ class MusicLibraryController
     puts "To play a song, enter 'play song'."
     puts "To quit, type 'exit'."
     puts "What would you like to do?"
-    unless i = "exit"
+    unless i != "exit"
       i = gets.chomp
     end
   end
